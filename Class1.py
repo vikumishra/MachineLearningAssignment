46421b0387b88c004b120ba09f9a70492a86611b
Vivek Mishra <vikumishra7073@gmail.com>
2018-08-08 10:04:04 +0530

    First Assignment Completed

---
+++ Class1.py
@@ -0,0 +1,16 @@
+#Question-1
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print("Desired Output Using Positive Slicing is")
+print(x[7:12])
+print("Desired Output Using Negative Slicing is")
+print(x[-9:-4])
+
+#Question-2
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print("Even Numbers using Slicing are:")
+print(x[1:16:2])
+
+#Question-3
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print("Numbers after skipping every fourth Number are")
+print(x[0:16:4])
